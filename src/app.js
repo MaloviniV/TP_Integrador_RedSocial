9a72846bc3544a9b776dec7d056f56370a6a16b2
MaloviniV <malovinivjd@hotmail.com>
2025-05-25 23:34:52 -0300

    pugs

--- src/app.js
+++ src/app.js
@@ -20,6 +20,33 @@ app.get("/login",(req,res)=>{
   res.render("login",{title:"Iniciar sesión"});
 });
 
+app.get("/home",(req,res)=>{
+  const publicaciones = [
+    {
+        usuario: "Juan Pérez",
+        resenia: "Excelente producto, superó mis expectativas.",
+        imagen: "/icons/user2.svg"
+    },
+    {
+        usuario: "María López",
+        resenia: "El servicio fue rápido y eficiente, lo recomiendo.",
+        imagen: "/icons/user.svg"
+    },
+    {
+        usuario: "Carlos Gómez",
+        resenia: "Buena calidad, aunque esperaba un poco más.",
+        imagen: "/icons/user.svg"
+    },
+    {
+        usuario: "Ana Martínez",
+        resenia: "Increíble experiencia, volveré a comprar sin duda.",
+        imagen: "/icons/user.svg"
+    }
+];
+
+  res.render("home",{title:"HOME",publicaciones});
+});
+
 app.listen(port,()=>{
   console.log(`Servidor escuchando en: http://localhost:${port}`);
 });
